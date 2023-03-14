b3f66c7f1a6792f1f49cc2d4a910e1c1c5a18b9d
hasan75 <hasanjab14@gmail.com>
2023-03-14 17:40:34 +0600

    intro edited

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,8 +19,13 @@ const Hero = () => {
             Hi, I'm <span className='text-[#915eff]'>Hasan Ahmed</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100 `}>
-            I develop 3D visuals, user <br className='sm:block hidden' />{' '}
-            interfaces and web applications.
+            I develop web applications, <br className='sm:block hidden' />
+            do research on <br className='sm:block hidden' />
+            <span className='text-[#915eff]'>
+              Machine Learning
+              <br className='sm:block hidden' />
+              and IoT.
+            </span>
           </p>
         </div>
       </div>
